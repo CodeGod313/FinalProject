6598ed6b49f17a2c168fef29916920341800a19d
Ales <codegod313@gmail.com>
2022-02-06 08:33:23 +0300

    Created Dao interface

--- src/main/java/by/epam/finalproject/dao/Dao.java
+++ src/main/java/by/epam/finalproject/dao/Dao.java
@@ -1,5 +1,13 @@
 package by.epam.finalproject.dao;
 
+import by.epam.finalproject.exception.DaoException;
+
+import java.sql.Connection;
+
 public interface Dao<T> {
-    T getById(Long id);
+    T getById(Long id) throws DaoException;
+    void create(T entity) throws DaoException;
+    void update(T entity) throws DaoException;
+    void delete(T entity) throws DaoException;
+    void setConnection(Connection connection);
 }
